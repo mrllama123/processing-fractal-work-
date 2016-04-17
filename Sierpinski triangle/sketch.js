aaaf29ef71a15232987387c9d3d05dc36f80ba32
Rowan Self <rowan.self@gmail.com>
2016-04-17 13:15:10 +1200

    fixed skech.js
    made sure the striangle method work

--- Sierpinski triangle/sketch.js
+++ Sierpinski triangle/sketch.js
@@ -1,32 +1,33 @@
 function setup() {
- size(500, 500);
+  createCanvas(700, 700);
   background(255);
   stroke(0);
   fill(255);
   noLoop();
+  
 }
 
 function draw() {
    striangle(250, 20, 460, 4);
 }
 
-function striangle(float x,float y,float h,int depth){
+function striangle(x,y,h,depth){
 	if(depth < 1)
 		return;
 	// draw the triangle
-    float a = sqrt((3 * h * h) / 4);
+    var a = sqrt((3 * h * h) / 4);
 	
     // YOUR CODE HERE - draw the triangle using, x, y, h and a
 	triangle(x,y,x+(a/2),h,x+a,y);
     // YOUR CODE HERE
     // work out the new h
     // reduce the depth
-    float half = h/2;
+    var half = a/2;
 	depth--;
     a = sqrt((3 * h * h) / 16); // helpful for working out new x and y
 	
     // YOUR CODE here - 3 recursive calls
     striangle(x,y,half,depth);
-    striangle(x-a,y,half,depth);
-    striangle(x,y-a,half,depth);
+    striangle(x+a,y,half,depth);
+    striangle(x,y+a,half,depth);
 }
